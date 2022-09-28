fb5c7ba4798291cb0e1ebaf0592056674968ff69
Sergi <sergivello18@gmail.com>
2022-09-28 22:58:37 +0200

    fix: solve error deploy with ga

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App () {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>Prueba 2</h1>
+                <h1>Prueba 3</h1>
                 <p> Esto se ha subido a partir de github actions</p>
                 <a
                     className="App-link"
