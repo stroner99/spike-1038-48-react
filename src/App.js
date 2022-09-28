fc5f8c2fc9309d7ba9d2a32f0e5c4655c1e02499
Sergi <sergivello18@gmail.com>
2022-09-28 22:38:09 +0200

    fix: try solve error deploy with ga

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App () {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>Prueba</h1>
+                <h1>Prueba 2</h1>
                 <p>
           Edit <code>src/App.js</code> and save to reload.
                 </p>
